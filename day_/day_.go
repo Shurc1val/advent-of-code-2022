c6b72695ccd1fa5bb9a2ff359857f52c3988f1bc
Shurc1val <hughpm16@gmail.com>
2024-04-03 12:04:43 +0100

    day template folder created

---
+++ day_/day_.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func part_one(input_string string) int64 {
+	return 0
+}
+
+
+func part_two(input_string string) int64 {
+	return 0
+}
+
+
+func main() {
+	puzzle_input, _ := os.ReadFile("puzzle_input.txt")
+	fmt.Println(part_one(string(puzzle_input)))
+	fmt.Println(part_two(string(puzzle_input)))
+}
---
+++ day_/day_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestPartOne(t *testing.T) {
+	input_string := ``
+	var expected_output int64 = 0
+
+	output := part_one(input_string)
+	if output != expected_output {
+		t.Fatalf("Incorrect")
+	}
+}
+
+
+func TestPartTwo(t *testing.T) {
+	input_string := ``
+	var expected_output int64 = 0
+	
+	output := part_one(input_string)
+	if output != expected_output {
+		t.Fatalf("Incorrect")
+	}
+}
